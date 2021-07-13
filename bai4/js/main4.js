4b7708ed1f4c51c87f8934cbf4b85054098e8291
Quoc Truong <tqt.ntml@gmail.com>
2021-07-13 21:17:03 +0700

    sua

--- bai4/js/main4.js
+++ bai4/js/main4.js
@@ -4,9 +4,9 @@ function taoDiv() {
 
   for (var i = 1; i < div10.length; i++) {
     if (i % 2 == 0) {
-      div10[i].style.background = "blue";
-    } else {
       div10[i].style.background = "red";
+    } else {
+      div10[i].style.background = "blue";
     }
     ketQua.style.display = "block";
   }
