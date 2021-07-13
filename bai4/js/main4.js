555207c6da5411db135b079e69052ce3f222ef5c
Quoc Truong <tqt.ntml@gmail.com>
2021-07-13 20:33:13 +0700

    update

--- bai4/js/main4.js
+++ bai4/js/main4.js
@@ -1,9 +1,9 @@
 function taoDiv() {
   var ketQua = document.getElementById("ketQua");
   var div10 = document.getElementsByTagName("div");
-  var span = document.createElement("span");
-  for (var i = 0; i < div10.length; i++) {
-    if ((i + 1) % 2 == 0) {
+
+  for (var i = 1; i < div10.length; i++) {
+    if (i % 2 == 0) {
       div10[i].style.background = "blue";
     } else {
       div10[i].style.background = "red";
